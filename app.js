33afdfe493dad9b3a05b306f47b951a5d2f8ba8d
cpplovelove <gsgslolo@naver.com>
2023-05-12 01:46:11 +0900

    [feat] socket 연결

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 import express from "express";
 import userController from "./src/user/userController.js";
 import stockController from "./src/stocks/stockController.js";
-
+import socketIo from "socket.io";
 const app = express();
 
 app.use(express.json());
 app.use("/user", userController);
 app.use("/stock", stockController);
 
-app.listen(8080, () => {
+const server = app.listen(5000, () => {
   console.log("server running on port 5000");
 });
 
-// http://localhost:8080/stock/buyStock
+const io = socketIo(server, {
+  transport: ["websocket"],
+  allowEIO3: true,
+});
+
+io.on("connect", (socket) => {
+  console.log(`클라이언트 연결 성공 - 소켓ID: ${socket.id}`);
+
+  socket.on("message", (data) => {
+    console.log(socket.id + "  " + data);
+    socket.emit("message ", { msg: "received" });
+  });
+});
